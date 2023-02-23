3b4bccf8ffc6a0857940af9790ddbfbbb6519d94
AB8939 <AB8939@student.jamk.fi>
2023-02-23 13:23:03 +0200

    added post

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,16 +1,35 @@
 const express = require('express')
 const router = express.Router()
+const Subscriber = require('../models/subscriber')
 
 //Getting all
-router.get('/', (req, res) => {
-  res.send('Hello World')
+router.get('/', async (req, res) => {
+  try {
+    const subscribers = await Subscriber.find()
+    res.json(subscribers)
+  } catch (err) {
+    res.status(500).json({ message: err.message })
+  }
 })
 
 //Getting one
-router.get('/:id', (req, res) => {})
+router.get('/:id', (req, res) => {
+  res.send(req.params.id)
+})
 
 //Creating one
-router.post('/', (req, res) => {})
+router.post('/', async (req, res) => {
+  const subscriber = new Subscriber({
+    name: req.body.name,
+    subscribedToChannel: req.body.subscribedToChannel,
+  })
+  try {
+    const newSubscriber = await subscriber.save()
+    res.status(201).json(newSubscriber)
+  } catch (err) {
+    res.status(400).json({ message: err.message })
+  }
+})
 
 //Updating one
 router.patch('/:id', (req, res) => {})
